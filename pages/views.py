9598205bae71c96d243ec81d467ab232b1644669
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-10-03 10:46:04 +0500

    simple crud finished

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
 
 
-class AboutPageView(TemplateView):
+class AboutPageView(ListView):
     template_name = 'about.html'
